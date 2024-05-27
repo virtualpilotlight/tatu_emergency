d542a983ae933e0dedc08cfcbd155c460d829ac0
Omega X Rae <63996271+virtualpilotlight@users.noreply.github.com>
2024-05-26 20:48:30 -0700

    Create emergency.sh

---
+++ emergency.sh
@@ -0,0 +1,4 @@
+!#/bin/bash
+
+wget -O movie.mp4  https://archive.org/download/Httpsweb.archive.orgweb20100207150524httpwww.youtube.comwatchv8mGBaXPlri8featurefvst/t.A.T.u.%20-%20All%20The%20Things%20She%20Said.mp4
+vlc movie.mp4 
